17234c1f093523392e745af939dabd1abb422a40
System Administrator <root@cb-sivananthams-MacBook-Pro.local>
2015-12-15 16:48:00 +0530

    Initial

---
+++ browser.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Login 2015 | PSG College of Technology</title>
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link rel="favicon" href="favicon.ico"/>
+</head>
+<body style="background:#EEE;color:#333;">
+<center style="margin-top:10%;">
+<h1>OUTDATED BROWSER DETECTED</h1>
+<h2>
+We built it using latest technology. Unfortunately, Your browser doesn't support those technologies.
+</h2>	
+<a href="//www.browsehappy.com">Update Your Browser</a>
+</center>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,50 @@
+<?php require_once("php/UserAgentParser.php");?>
+<!-- AUTHOR : Sivanantham Gnanavel (13MX45) -->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Login 2015 | PSG College of Technology</title>
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link rel="stylesheet" type="text/css" href="css/styles.css"/>
+	<link rel="favicon" href="favicon.ico"/>
+</head>
+<body>
+
+	<div class="social" id="social">
+	<div id="fb-root"></div>
+	<div class="fb-like" data-href="https://www.facebook.com/psg.symposium" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+	</div>
+	
+
+	<div id="container" class="container">
+		<ul id="scene" class="scene" data-limit-y="0">
+		
+			<li class="layer" data-depth="0.10"><div class="img-road"></div>  </li>
+			<li class="layer" data-depth="0.20"><div class="img-city"></div>  </li>
+			
+			<li class="layer" data-depth="0.40"><div class="img-grain"></div>  </li>
+			<li class="layer" data-depth="0.40"><div class="img-15 swing-6"></div>  </li>
+			
+			<li class="layer" data-depth="0.30"><div class="img-o swing-1"></div>  </li>
+			<li class="layer" data-depth="0.30"><div class="img-i swing-2"></div>  </li>
+				
+			<li class="layer" data-depth="0.40"><div class="img-l swing-3"></div>  </li>
+			<li class="layer" data-depth="0.60"><div class="img-g swing-4"></div>  </li>
+			<li class="layer" data-depth="0.60"><div class="img-n swing-5"></div>  </li>
+			
+			
+			<li class="layer" data-depth="1.00"><div class="img-lorry"></div>  </li>	
+			<li class="layer" data-depth="2.00"><div class="img-14"></div>  </li>	
+			<li class="layer" data-depth="3.00"><div class="img-car"></div>  </li>
+		</ul>
+	</div>
+
+	<div class="img-tag"></div>
+	
+	<script src="js/parallax.min.js"></script>
+	<script src="js/script.js"></script>
+	
+</body>
+</html>
